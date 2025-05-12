a11582769fe7b264cbb34dc46f809c0eb0215936
13lackade <13lackade@gmail.com>
2025-05-13 00:40:35 +0900

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,6 @@ pushd ~/.local/share/dotfiles/deno
 ~/.dotfiles/deno/dot_config/plug/deno/deno.sh
 popd
 
-export PATH="$PATH:~/.local/bin"
+export PATH="$PATH:$HOME/.local/bin"
 ~/.dotfiles/scripts/dot_local/bin/dot
 ~/.dotfiles/scripts/dot_local/bin/plug
