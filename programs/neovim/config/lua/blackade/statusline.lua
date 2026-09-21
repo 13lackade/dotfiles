8713b6a5ad8065b618ded4347d77f6bcfc4849ae
13lackade <13lackade@gmail.com>
2026-09-21 10:20:42 +0900

    update statusline

--- programs/neovim/config/lua/blackade/statusline.lua
+++ programs/neovim/config/lua/blackade/statusline.lua
@@ -14,12 +14,29 @@ local mode_color = function()
     return mode_colors[mode] or 1
 end
 
+local get_diagnostics = function()
+    local counts = vim.diagnostic.count(0)
+
+    local errors = counts[vim.diagnostic.severity.ERROR] or 0
+    local warnings = counts[vim.diagnostic.severity.WARN] or 0
+    local info = counts[vim.diagnostic.severity.INFO] or 0
+    local hints = counts[vim.diagnostic.severity.HINT] or 0
+
+    return table.concat({
+        '%#DiagnosticError#\u{ea87} ' .. errors .. '%*  ',
+        '%#DiagnosticWarn#\u{ea6c} ' .. warnings .. '%*  ',
+        '%#DiagnosticInfo#\u{eb32} ' .. info .. '%*  ',
+        '%#DiagnosticHint#\u{ea74} ' .. hints .. '%*  ',
+    })
+end
+
 vim.g.customstatusline = function()
     local edge = string.format('%%%i* %%*', mode_color())
     return table.concat({
         edge,
         vim.bo.filetype ~= 'netrw' and ' %t %m' or ' netrw',
         '%=',
+        #vim.lsp.get_clients({ bufnr = 0 }) ~= 0 and get_diagnostics() or '',
         edge,
     })
 end
